d43acb334409c1f1dbf21851cb1ac7209bb0370b
owen <owservera@gmail.com>
2026-03-01 22:40:00 +0100

    fix(docs): add trailing slash to baseUrl per Docusaurus spec

--- vivim.docs.context/docusaurus.config.ts
+++ vivim.docs.context/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://vivim.live',
-  baseUrl: '/docs',
+  baseUrl: '/docs/',
 
   organizationName: 'vivim',
   projectName: 'vivim-live',
