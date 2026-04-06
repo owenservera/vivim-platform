0855161b21bc9e7951fefdc443027d302326dd55
Owen Alexander Wallace Servera <owenservera@users.noreply.github.com>
2026-04-06 17:24:05 +0200

    chore: moderate repo cleanup - remove debug artifacts and reorganize docs

--- server/test-resolve.js
+++
@@ -1,5 +0,0 @@
-import { getPrismaClient } from './src/lib/database.js';
-console.log('Successfully imported database.js');
-const prisma = getPrismaClient();
-console.log('Prisma client obtained');
-process.exit(0);
